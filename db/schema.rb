fd7876237c12e89a7d95b54b8671fd793b9356b0
Pedro Lima <pedrogdcl@gmail.com>
2025-07-29 18:20:07 -0400

    Create tool table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_29_214450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "tools", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_tools_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
