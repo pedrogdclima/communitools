a4062e33cc7135b60d7009d809beec715b204902
Pedro Lima <pedrogdcl@gmail.com>
2025-11-21 13:09:34 -0500

    Add routes and views for home, user profile, and tool list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "tools", to: "users#tools"
+
+  get "users", to: "users#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
