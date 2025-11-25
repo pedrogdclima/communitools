7731aeb81c5f6fd17f812a58147f68e9dc3bbb1a
Pedro Lima <pedrogdcl@gmail.com>
2025-11-25 16:55:32 -0500

    Allow user to add new tools

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -3,12 +3,26 @@ class ToolsController < ApplicationController
     @tool = Tool.find(params[:id])
   end
 
-  def create
-  end
-
   def new
+    @tool = Tool.new
   end
 
-  def edit
+  def create
+    @tool = Tool.new(tool_params)
+    @tool.user_id = current_user.id
+    if @tool.save
+      redirect_to "/"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # def edit
+  # end
+
+  private
+
+  def tool_params
+    params.expect(tool: [ :name, :brand, :desc ])
   end
 end
