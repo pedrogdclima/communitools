a0a19d3d6ca181f1c36ec7e32c6486ecc25b9892
Pedro Lima <pedrogdcl@gmail.com>
2025-12-03 11:21:20 -0500

    Show marker when user has latlng set

--- app/javascript/slippy_map.js
+++ app/javascript/slippy_map.js
@@ -1,27 +1,40 @@
 
-console.log('Hello from slippy map');
-
 let map;
 let marker = L.marker();
 let latlngField;
+
 document.addEventListener('DOMContentLoaded', () => {
-  map = L.map('map').setView([43.6529148, -79.3853107], 11.5);
   latlngField = document.querySelector('#user_latlng');
+  let initialCoord = latlngField.value;
+  
+  if (initialCoord) {
+    // "43.6686, -79.3248"
+    map = L.map('map').setView(initialCoord.split(', '), 11.5);
+    setMarker(initialCoord.split(', '));
+  } else {
+    map = L.map('map').setView([43.6529148, -79.3853107], 11.5);
+  }
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
+
   map.on('click', onMapClick);
 });
+
+function setMarker(latlng) {
+  marker
+    .setLatLng(latlng)
+    .addTo(map);
+}
+
 function parseLatlng(number) {
   return number.toFixed(4);
 }
+
 function onMapClick(event) {
-  console.log('click');
-  marker
-    .setLatLng(event.latlng)
-    .addTo(map);
+  setMarker(event.latlng);
   
   let location = [event.latlng.lat, event.latlng.lng].map(parseLatlng);
   latlngField.value = location.join(', ');
