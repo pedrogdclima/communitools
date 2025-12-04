6b86f10191bca47297ff8194ed846d1c3110e7fd
Pedro Lima <pedrogdcl@gmail.com>
2025-12-04 17:43:40 -0500

    Restrict users from edit and delete other users tools

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,6 +1,10 @@
 class ToolsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_tool, only: %i[ show edit update destroy ]
-  
+  before_action only: %i[ edit update destroy ] do
+    is_current_user_owner
+  end
+
   def show
   end
 
@@ -42,5 +46,13 @@ class ToolsController < ApplicationController
 
   def tool_params
     params.expect(tool: [ :name, :brand, :desc ])
+  end
+
+  def is_current_user_owner
+    if @tool.nil? || @tool.user_id.nil?
+      redirect_to users_path , alert: "Tool doesn't exist!"
+    elsif @tool.user_id != current_user.id
+      redirect_to tool_path(@tool.id), alert: t("error.messages.wrong_user")
+    end
   end
 end
