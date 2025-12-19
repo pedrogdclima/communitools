2320d179b22cfcf1904f077181f3752c8e2046a2
Pedro <pedro@gmail.com>
2025-12-19 15:54:46 -0500

    Add more seed data for better testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,13 @@
   #   MovieGenre.find_or_create_by!(name: genre_name)
   # end
 
-User.create!(username: 'pedrolima', name: 'Pedro', email: 'pedro.lima@server.com', password: 'pedrolima')
+User.create!(username: 'pedrolima', name: 'Pedro', email: 'pedro@server.com', password: 'password', latlng: '43.6652, -79.3284')
 
-User.create!(username: 'toriejw', name: 'Torie', email: 'victoriajw@server.com', password: 'toriejw')
+User.create!(username: 'maggieclark', name: 'Maggie', email: 'maggie@server.com', password: 'password', latlng: '43.6623, -79.3255')
 
-User.create!(username: 'avethebabe', name: 'Avery', email: 'avedl@server.com', password: 'avethebabe')
+User.create!(username: 'avethebabe', name: 'Avery', email: 'avery@server.com', password: 'password', latlng: '43.6947, -79.3976')
+
+User.create!(username: 'nickchalis', name: 'Nick', email: 'nick@server.com', password: 'password', latlng: '43.6659, -79.3454')
 
 [
   {name: 'Mallet', brand: '', desc:'', user_id: '3'},
@@ -22,6 +24,10 @@ User.create!(username: 'avethebabe', name: 'Avery', email: 'avedl@server.com', p
   {name: 'Oscillating Multitool', brand: 'Dewalt', desc:'', user_id: '1'},
   {name: 'Chain Breaker', brand: 'Shimano', desc:'', user_id: '1'},
   {name: 'Crank Puller', brand: 'Park Tool', desc:'', user_id: '1'},
+  {name: 'Truing Stand', brand: 'Park Tool', desc:'', user_id: '4'},
+  {name: 'Car Jack', brand: 'Mastercraft', desc:'', user_id: '4'},
+  {name: 'Torque Wrench', brand: '', desc:'', user_id: '4'},
+  {name: 'Air Compressor', brand: '', desc:'', user_id: '4'},
 ].each do |tool|
   Tool.create!(tool)
 end
