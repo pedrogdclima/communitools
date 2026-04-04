6141659d16b013c7d31ac41d13ed0cfd7f083dd3
pedrogdclima <pedrogdcl@gmail.com>
2026-04-04 11:28:50 -0400

    add flash messages on successful operation

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -16,7 +16,7 @@ class ToolsController < ApplicationController
     @tool = Tool.new(tool_params)
     @tool.user_id = current_user.id
     if @tool.save
-      redirect_to "/"
+      redirect_to "/", notice: t(".created")
     else
       render :new, status: :unprocessable_entity
     end
@@ -24,7 +24,7 @@ class ToolsController < ApplicationController
 
   def destroy
     @tool.destroy
-    redirect_to "/"
+    redirect_to "/", notice: t(".deleted")
   end
 
   def edit
@@ -32,7 +32,7 @@ class ToolsController < ApplicationController
 
   def update
     if @tool.update(tool_params)
-      redirect_to "/"
+      redirect_to "/", notice: t(".updated")
     else
       render :edit, status: :unprocessable_entity
     end
